b1dbf57478bd9c41bc67f44b864eecd600125f72
raminhrd <raminhr.work@gmail.com>
2026-01-31 22:11:22 +0100

    add permission for specialist 🙈

---
+++ users/permission.py
@@ -0,0 +1,16 @@
+from rest_framework.permissions import BasePermission
+from users.models import User
+
+class IsApprovedSpecialist(BasePermission):
+    message = "Your specialist account is not approved yet."
+
+    def has_permission(self, request, view):
+        user = request.user
+        if not user or not user.is_authenticated:
+            return False
+
+        if user.role != User.RoleChoices.SPECIALIST:
+            return False
+
+        sr = getattr(user, "specialist_request", None)
+        return sr is not None and sr.status == sr.Status.APPROVED
